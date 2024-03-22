c134898c6392d10f7ff6fe24b568e13b0d3b6f60
Matěj Votruba <matej.votruba@depoto.cz>
2024-03-22 12:05:33 +0100

    Rename job table name

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -9,7 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: JobRepository::class)]
-#[ORM\Table(name: "job")]
+#[ORM\Table(name: "job_queue")]
 class Job
 {
     const STATUS_PLANNED = 'planned';
