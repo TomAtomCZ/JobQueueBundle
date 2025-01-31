6957a61755f4f58c64f3508a4a663e67317f29b0
Matěj Votruba <matej.votruba@depoto.cz>
2025-01-31 15:07:39 +0100

    Update templates - mobile friendly, add pagination to job lists

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -3,10 +3,10 @@
 namespace TomAtom\JobQueueBundle\Controller;
 
 use DateTimeImmutable;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Exception\ORMException;
 use Doctrine\ORM\OptimisticLockException;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,13 +22,12 @@ use TomAtom\JobQueueBundle\Service\CommandJobFactory;
 #[Route(path: '/job')]
 class JobController extends AbstractController
 {
-    private EntityManager $entityManager;
-    private TranslatorInterface $translator;
-
-    public function __construct(EntityManagerInterface $entityManager, TranslatorInterface $translator)
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly TranslatorInterface    $translator,
+        private readonly PaginatorInterface     $paginator
+    )
     {
-        $this->entityManager = $entityManager;
-        $this->translator = $translator;
     }
 
     /**
@@ -61,46 +60,62 @@ class JobController extends AbstractController
     }
 
     /**
+     * @param Request $request
      * @param int|null $id - Related entity id
      * @param string|null $name - Related entity class name (self::class)
      * @return Response
      */
     #[IsGranted(JobQueuePermissions::ROLE_JOB_LIST)]
     #[Route(path: '/list/{id?}/{name?}', name: 'job_queue_list')]
-    public function list(?int $id = null, string $name = null): Response
+    public function list(Request $request, ?int $id = null, ?string $name = null): Response
     {
         if (!empty($name) && !empty($id)) {
             $entity = $this->entityManager->getRepository($name)->find($id);
         }
 
-        $jobs = $this->entityManager
-            ->createQueryBuilder()
-            ->select('j')
-            ->from(Job::class, 'j');
+        $jobs = $this->entityManager->getRepository(Job::class)
+            ->createQueryBuilder('j');
 
         if (!empty($id)) {
             $jobs = $jobs->where('j.relatedEntityId = :id')
                 ->setParameter('id', $id);
         }
 
-        $jobs = $jobs->orderBy('j.createdAt', 'DESC')
-            ->getQuery()
-            ->getResult();
+        $jobs = $jobs->orderBy('j.createdAt', 'DESC');
+
+        $pagination = $this->paginator->paginate(
+            $jobs->getQuery(),
+            $request->query->getInt('page', 1),
+            50
+        );
+        $jobs = $pagination->getItems();
 
         return $this->render('@JobQueue/job/list.html.twig', [
             'jobs' => $jobs,
             'relatedEntityId' => $id,
-            'relatedEntity' => $entity ?? null
+            'relatedEntity' => $entity ?? null,
+            'pagination' => $pagination
         ]);
     }
 
     #[IsGranted(JobQueuePermissions::ROLE_JOB_LIST)]
     #[Route(path: '/recurring/list', name: 'job_queue_recurring_list')]
-    public function recurringList(): Response
+    public function recurringList(Request $request): Response
     {
-        $jobs = $this->entityManager->getRepository(JobRecurring::class)->findBy([], ['createdAt' => 'DESC']);
+        $jobs = $this->entityManager->getRepository(JobRecurring::class)
+            ->createQueryBuilder('jr')
+            ->orderBy('jr.createdAt', 'DESC');
+
+        $pagination = $this->paginator->paginate(
+            $jobs->getQuery(),
+            $request->query->getInt('page', 1),
+            50
+        );
+        $jobs = $pagination->getItems();
+
         return $this->render('@JobQueue/job/recurring_list.html.twig', [
-            'jobs' => $jobs
+            'jobs' => $jobs,
+            'pagination' => $pagination
         ]);
     }
 
@@ -133,6 +148,27 @@ class JobController extends AbstractController
             'id' => $request->query->get('listId'),
             'name' => $request->query->get('listName')
         ]);
+    }
+
+    #[IsGranted(JobQueuePermissions::ROLE_JOB_DELETE)]
+    #[Route(path: '/delete-recurring/{id<\d+>}', name: 'job_queue_delete_recurring')]
+    public function deleteRecurring(?JobRecurring $job, Request $request): Response
+    {
+        if (empty($job)) {
+            $this->addFlash('warning', $this->translator->trans('job.detail.error.not_found'));
+            return $this->redirectToRoute('job_queue_recurring_list');
+        }
+
+        // Try to delete recurring job
+        try {
+            $this->entityManager->remove($job);
+            $this->entityManager->flush();
+            $this->addFlash('success', $this->translator->trans('job.deletion.success'));
+        } catch (ORMException $e) {
+            $this->addFlash('danger', $this->translator->trans('job.deletion.error') . $e->getMessage());
+        }
+
+        return $this->redirectToRoute('job_queue_recurring_list');
     }
 
     #[IsGranted(JobQueuePermissions::ROLE_JOB_CANCEL)]
@@ -184,7 +220,7 @@ class JobController extends AbstractController
 
         // Try to create the command job
         try {
-            $job = $commandJobFactory->createCommandJob($parentJob->getCommand(), $parentJob->getCommandParams(), $listId, $listName, $parentJob, $parentJob->getJobRecurringParent(), $parentJob->getStartAt());
+            $job = $commandJobFactory->createCommandJob($parentJob->getCommand(), $parentJob->getCommandParams(), $listId, $listName, $parentJob, $parentJob->getJobRecurringParent());
             $this->addFlash('success', $this->translator->trans('job.creation.success'));
         } catch (OptimisticLockException|ORMException|CommandJobException $e) {
             // Redirect back to the command schedule
